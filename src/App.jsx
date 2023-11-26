d4a059d8bfc9474408da84d47dd116bf07e58314
KevinLaird <kevinlaird16@gmail.com>
2023-11-25 19:41:33 -0800

    Added loading functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
+import { useState, useEffect } from 'react';
 import Landing from './components/Landing';
 import './App.css';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+
+  useEffect(() => {
+    if (!loading) {
+      console.log('Animation triggered');
+    }
+  }, [loading]);
+
   return (
     <>
-      <Landing />
+      {loading ? (
+        <div className="h-screen flex flex-col gap-5 items-center justify-center">
+          <img
+            src="./assets/commlead-logo.png"
+            className="animate-[spin_3s_linear_infinite]"
+            alt="Loader"
+          />
+          <p className="font-black font-encode text-3xl">Loading...</p>
+        </div>
+      ) : (
+        <Landing />
+      )}
     </>
   );
 }
