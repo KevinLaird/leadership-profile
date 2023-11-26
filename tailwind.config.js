806b158d8d651644632b88a523ca6d48e209a112
KevinLaird <kevinlaird16@gmail.com>
2023-11-25 19:40:09 -0800

    Added commlead fonts to theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      encode: ['Encode Sans', 'sans-serif'],
+      sans: ['Open Sans', 'sans-serif'],
+    },
+    extend: {
+      animation: {
+        'spin-slow': 'spin 3s linear infinite',
+      },
+    },
   },
   plugins: [],
 };
