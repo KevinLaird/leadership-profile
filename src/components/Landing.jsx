35a452f6c3a615cc26f616677ed384be946184f1
KevinLaird <kevinlaird16@gmail.com>
2023-11-25 20:38:56 -0800

    Redesigned landing

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,42 +1,24 @@
+import KeyboardDoubleArrowDownRoundedIcon from '@mui/icons-material/KeyboardDoubleArrowDownRounded';
+
 export default function Landing() {
   return (
-    <div className="bg-white">
-      <header className="absolute inset-x-0 top-0 z-50"></header>
-
-      <div className="relative isolate px-6 pt-14 lg:px-8">
-        <div
-          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-          aria-hidden="true"
-        >
-          <div
-            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-            style={{
-              clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-            }}
-          />
-        </div>
-        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-          <div className="text-center">
-            <h1 className="text-5xl font-black font-encode">Kevin Laird</h1>
-            <p className="text-2xl mt-6 font-sans text-gray-600">
-              A project for Communication Leadership: Leadership at All Levels,
-              taught by Kelley Myers.
-            </p>
-          </div>
-        </div>
-        <div
-          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-          aria-hidden="true"
-        >
-          <div
-            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-            style={{
-              clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-            }}
-          />
-        </div>
+    <div className="h-screen bg-white relative flex flex-col gap-5 items-center justify-center">
+      <img
+        src="./public/assets/commlead-logo-4x.png"
+        alt="University of Washington Communication Leadership Logo"
+        className="absolute"
+      />
+      <div className="z-40 text-center">
+        <h1 className="text-9xl font-black font-encode">Kevin Laird</h1>
+        <h2 className="text-5xl font-bold font-encode text-gray-950">
+          Leadership Profile
+        </h2>
+      </div>
+      <div className="animate-bounce-slow absolute bottom-0 pb-5">
+        <KeyboardDoubleArrowDownRoundedIcon
+          sx={{ fontSize: 72 }}
+          color="action"
+        />
       </div>
     </div>
   );
