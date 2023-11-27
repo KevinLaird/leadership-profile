0b1e610f6149adff7f743e8a53ce0e7a4e8a49da
KevinLaird <kevinlaird16@gmail.com>
2023-11-27 15:50:24 -0800

    Introduction is responsive now

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -5,14 +5,15 @@ const Introduction = () => {
     <div
       className={`relative ${styles.contentBox} flex items-center justify-center`}
     >
-      <div className={`absolute top-0 ${styles.BGBars}`} />{' '}
-      <div className="relative items-center justify-center z-20 flex gap-20">
+      <div className={`absolute top-0 ${styles.BGBars}`} />
+      <div className="relative items-center justify-center z-20 flex flex-col lg:flex-row gap-20">
         <img
           src="./assets/Profile.png"
           alt="Profile picture of Kevin Laird smiling. Ain't he cute?"
+          className="w-full max-w-md max-h-60 lg:max-h-full object-contain" // Preserve aspect ratio
         />
         <div className="flex flex-col gap-10">
-          <h1 className="font-haettenschweiler text-4xl">
+          <h1 className="font-haettenschweiler text-4xl text-center lg:text-left">
             Leading from behind...
           </h1>
           <p className={`font-robotolight text-2xl ${styles.content}`}>
