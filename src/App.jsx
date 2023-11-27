c96a85b69002e6e427c64cd084493ea817df8da9
KevinLaird <kevinlaird16@gmail.com>
2023-11-27 15:50:50 -0800

    Added components to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { useState, useEffect } from 'react';
 import Landing from './components/Landing';
-import './App.css';
+import styles from './App.module.css';
 import Introduction from './components/Introduction';
-import LeadershipValues from './components/LeadershipValues';
+import Tabs from './components/Tabs';
+import TabContent from './components/TabContent';
+import TitleBanner from './components/TitleBanner';
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -14,10 +16,74 @@ function App() {
     }, 2000);
   }, []);
 
+  const funContent = [
+    'I bring fun and energy to everything I do. If I’m excited about something, I show it and I do my best to ensure everyone else is having some form of fun with any activity I am a part of.',
+  ];
+
+  // const empathyContent = [
+  //   'As a transformational leader, I guide with collaboration, effective communication, continuous learning, and the well-being of my team in mind.',
+  //   'Valuing empathy, fun, and stability, I create a predictable and engaging environment where everyone feels safe and comfortable to contribute their skills to the team.',
+  //   'My purpose is to connect people together to create a space that is filled with compassion and empathy by being an effective communicator and a patient leader.',
+  // ];
+
+  // const kindnessContent = [
+  //   'As a transformational leader, I guide with collaboration, effective communication, continuous learning, and the well-being of my team in mind.',
+  //   'Valuing empathy, fun, and stability, I create a predictable and engaging environment where everyone feels safe and comfortable to contribute their skills to the team.',
+  //   'My purpose is to connect people together to create a space that is filled with compassion and empathy by being an effective communicator and a patient leader.',
+  // ];
+
+  // const patienceContent = [
+  //   'As a transformational leader, I guide with collaboration, effective communication, continuous learning, and the well-being of my team in mind.',
+  //   'Valuing empathy, fun, and stability, I create a predictable and engaging environment where everyone feels safe and comfortable to contribute their skills to the team.',
+  //   'My purpose is to connect people together to create a space that is filled with compassion and empathy by being an effective communicator and a patient leader.',
+  // ];
+
   const tabs = [
-    { label: 'Tab 1', content: <p>Content for Tab 1</p> },
-    { label: 'Tab 2', content: <p>Content for Tab 2</p> },
-    { label: 'Tab 3', content: <p>Content for Tab 3</p> },
+    {
+      label: 'fun',
+      content: (
+        <TabContent
+          imageSrc="/assets/kevinWithABird.png"
+          imageAlt="Kevin with his 'lil mascot Echo, the Sun Conure (bird). So fun!"
+          imageClassName="your-custom-image-class"
+          title="fun"
+          paragraphs={funContent}
+        />
+      ),
+    },
+    {
+      label: 'empathy',
+      content: (
+        <TabContent
+          imageSrc=""
+          imageAlt="Profile picture of Kevin Laird smiling. Ain't he cute?"
+          imageClassName="your-custom-image-class"
+          paragraphs={funContent}
+        />
+      ),
+    },
+    {
+      label: 'kindness',
+      content: (
+        <TabContent
+          imageSrc=""
+          imageAlt="Profile picture of Kevin Laird smiling. Ain't he cute?"
+          imageClassName="your-custom-image-class"
+          paragraphs={funContent}
+        />
+      ),
+    },
+    {
+      label: 'patience',
+      content: (
+        <TabContent
+          imageSrc=""
+          imageAlt="Profile picture of Kevin Laird smiling. Ain't he cute?"
+          imageClassName="your-custom-image-class"
+          paragraphs={funContent}
+        />
+      ),
+    },
   ];
 
   return (
@@ -37,10 +103,14 @@ function App() {
             <Landing />
           </header>
           <section>
+            <TitleBanner title="personal introduction" />
             <Introduction />
           </section>
           <section>
-            <LeadershipValues tabs={tabs} />
+            <TitleBanner title="leadership values" />
+            <div className={styles.Values}>
+              <Tabs tabs={tabs} />
+            </div>
           </section>
         </>
       )}
