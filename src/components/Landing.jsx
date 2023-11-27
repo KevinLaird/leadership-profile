866897342f31de7f576346d65efe0a592959b7b4
KevinLaird <kevinlaird16@gmail.com>
2023-11-27 15:50:08 -0800

    Refined landing styles

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -7,10 +7,8 @@ export default function Landing() {
       className={`relative flex flex-col gap-5 items-center justify-center ${styles.bg}`}
     >
       <div className={`z-40 text-center ${styles.bgTitle}`}>
-        <h2 className={`font-haettenschweiler text-5xl ${styles.h2}`}>
-          Kevin Laird
-        </h2>
-        <h1 className={`font-haettenschweiler text-9xl ${styles.h1}`}>
+        <h2 className={`font-haettenschweiler ${styles.h2}`}>Kevin Laird</h2>
+        <h1 className={`font-haettenschweiler ${styles.h1}`}>
           Leadership Profile
         </h1>
       </div>
