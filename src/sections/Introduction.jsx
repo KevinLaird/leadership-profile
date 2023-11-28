748a8abe73b7152c8cc941a866e50bcc1f88c0b6
KevinLaird <kevinlaird16@gmail.com>
2023-11-28 12:55:48 -0800

    Changed title text

--- src/sections/Introduction.jsx
+++ src/sections/Introduction.jsx
@@ -9,21 +9,21 @@ const Introduction = () => {
       <div className="relative items-center justify-center z-20 flex flex-col lg:flex-row gap-20">
         <img
           src="./assets/Profile.png"
-          alt="Profile picture of Kevin Laird smiling."
+          alt="Profile of Kevin Laird smiling."
           className="w-full max-w-md max-h-60 lg:max-h-full object-contain" // Preserve aspect ratio
         />
         <div className="flex flex-col gap-10">
           <h1 className="font-haettenschweiler text-4xl text-center lg:text-left">
-            Leading from behind...
+            Transformational Leadership
           </h1>
           <p className={`font-robotolight text-2xl ${styles.content}`}>
-            As a transformational leader, I guide with collaboration, effective
-            communication, continuous learning, and the well-being of my team in
-            mind. Valuing empathy, fun, and stability, I create a predictable
-            and engaging environment where everyone feels safe and comfortable
-            to contribute their skills to the team. My purpose is to connect
-            people together to create a space that is filled with compassion and
-            empathy by being an effective communicator and a patient leader.
+            As a leader, I guide with collaboration, effective communication,
+            continuous learning, and the well-being of my team in mind. Valuing
+            empathy, fun, and stability, I create a predictable and engaging
+            environment where everyone feels safe and comfortable to contribute
+            their skills to the team. My purpose is to connect people together
+            to create a space that is filled with compassion and empathy by
+            being an effective communicator and a patient leader.
           </p>
         </div>
       </div>
