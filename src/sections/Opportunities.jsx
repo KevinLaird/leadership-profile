8832865433fd7c4777c95636cdd9fd5e787ac875
KevinLaird <kevinlaird16@gmail.com>
2023-11-28 12:57:04 -0800

    Working on opportunities section

--- src/sections/Opportunities.jsx
+++ src/sections/Opportunities.jsx
@@ -2,8 +2,8 @@ const Opportunities = () => {
   return (
     <div>
       <img
-        src="./assets/Profile.png"
-        alt="Profile picture of Kevin Laird smiling. Ain't he cute?"
+        src="./assets/connectionsGraphic.png"
+        alt="Graphic communicating connecting with professionals in the industry. Large orange sphere connected to two smaller yellow spheres."
         className="w-full max-w-md max-h-60 lg:max-h-full object-contain" // Preserve aspect ratio
       />
     </div>
