09ccf5336857a70e28cf1fe332324d10911ef5c6
KevinLaird <kevinlaird16@gmail.com>
2023-11-27 16:22:19 -0800

    Initialized opportunities section

---
+++ src/sections/Opportunities.jsx
@@ -0,0 +1,13 @@
+const Opportunities = () => {
+  return (
+    <div>
+      <img
+        src="./assets/Profile.png"
+        alt="Profile picture of Kevin Laird smiling. Ain't he cute?"
+        className="w-full max-w-md max-h-60 lg:max-h-full object-contain" // Preserve aspect ratio
+      />
+    </div>
+  );
+};
+
+export default Opportunities;
