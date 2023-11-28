39974b08eceda3803daef5e5ab612aa0ad7d27a3
KevinLaird <kevinlaird16@gmail.com>
2023-11-28 15:47:28 -0800

    Added opportunities section

--- src/sections/Opportunities.jsx
+++ src/sections/Opportunities.jsx
@@ -1,11 +1,20 @@
+import ImageWithListItem from '../components/ImageWithListItem';
+import styles from './Opportunities.module.css';
+
 const Opportunities = () => {
+  const imageSource = '/assets/connectionsGraphic.png'; // Provide the actual image URL
+  const listItems = [
+    'Community Workshops',
+    'Udemy',
+    'Coursera',
+    'LinkedIn Learning',
+    'Career Advising with Lynn Juve',
+    'Informational Interviews',
+  ]; // Provide your list items
+
   return (
-    <div>
-      <img
-        src="./assets/connectionsGraphic.png"
-        alt="Graphic communicating connecting with professionals in the industry. Large orange sphere connected to two smaller yellow spheres."
-        className="w-full max-w-md max-h-60 lg:max-h-full object-contain" // Preserve aspect ratio
-      />
+    <div className={styles.container}>
+      <ImageWithListItem imageSrc={imageSource} listItem={listItems} />
     </div>
   );
 };
