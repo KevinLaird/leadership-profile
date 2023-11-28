410884a08078f2ea51adaa531a057c9fb95f27f0
KevinLaird <kevinlaird16@gmail.com>
2023-11-28 15:48:57 -0800

    Dummy text has changed to new dummy text

--- src/sections/Dreams.jsx
+++ src/sections/Dreams.jsx
@@ -7,7 +7,10 @@ const Dreams = () => {
       imageSrc: '/assets/community.png',
       imageAlt: '...',
       title: 'Community',
-      paragraphs: ['Paragraph 3', 'Paragraph 4'],
+      paragraphs: [
+        'Lorem ipsum dolor sit amet consectetur. Pellentesque ipsum et eget fermentum. Velit senectus ut vitae nullam tellus pellentesque auctor. Lacus est erat duis sed amet libero at vulputate tellus. Eu egestas sit fringilla ullamcorper duis ullamcorper. Morbi tincidunt et cras aliquam urna tincidunt. Sollicitudin pulvinar tempus varius quam diam nec commodo. Feugiat quis viverra donec adipiscing in montes et nisi eu. Vel lorem lacus interdum sed.',
+        'Aliquam est turpis mauris aliquam pellentesque. At semper sed id lectus tristique purus morbi scelerisque tristique. Adipiscing est facilisi malesuada posuere. Sed odio dictum mattis nulla urna scelerisque. Amet feugiat.',
+      ],
     },
     {
       imageSrc: '/assets/career.png',
