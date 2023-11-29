98814b9dc7d5e515ebb08e97f6fca336c00d7027
KevinLaird <kevinlaird16@gmail.com>
2023-11-28 16:16:30 -0800

    Hoefully fixed responsive issues

--- src/components/TitleBanner.jsx
+++ src/components/TitleBanner.jsx
@@ -4,9 +4,7 @@ import styles from './TitleBanner.module.css';
 const TitleBanner = ({ title }) => {
   return (
     <div className={styles.box}>
-      <h1
-        className={`font-haettenschweiler text-center text-4xl sm:text-6xl lg:text-8xl xl:text-9xl ${styles.title}`}
-      >
+      <h1 className={`font-haettenschweiler text-center ${styles.title}`}>
         {title}
       </h1>
     </div>
